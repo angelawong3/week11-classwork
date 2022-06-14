6bdf2232e02895fcd686ab27bcd29c48dd3bbfec
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-14 21:02:49 +0100

    ex4

--- 4/server.js
+++ 4/server.js
@@ -10,7 +10,7 @@ const PORT = 3001;
 const app = express();
 
 // TODO: Create a route for a GET request that will return the content of our `terms.json` file
-app.get("/terms", (req, res) => res.json(termData));
+app.get("/api/terms", (req, res) => res.json(termData));
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
