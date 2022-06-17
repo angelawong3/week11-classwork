782988f09ad774443076edc5b80570aed114587e
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-17 14:47:31 +0100

    complete ex20

--- 20/server.js
+++ 20/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const fs = require("fs").promises;
 // Helper method for generating unique ids
-const uuid = require('./helpers/uuid');
+const uuid = require("./helpers/uuid");
 
 const PORT = 3001;
 
@@ -11,14 +11,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 // GET request for reviews
-app.get('/api/reviews', (req, res) => {
+app.get("/api/reviews", (req, res) => {
   // Send a message to the client
   res.json(`${req.method} request received to get reviews`);
 
@@ -27,7 +27,7 @@ app.get('/api/reviews', (req, res) => {
 });
 
 // POST request to add a review
-app.post('/api/reviews', (req, res) => {
+app.post("/api/reviews", async (req, res) => {
   // Log that a POST request was received
   console.info(`${req.method} request received to add a review`);
 
@@ -48,23 +48,41 @@ app.post('/api/reviews', (req, res) => {
     const reviewString = JSON.stringify(newReview);
 
     // Write the string to a file
-    fs.writeFile(`./db/reviews.json`, reviewString, (err) =>
-      err
-        ? console.error(err)
-        : console.log(
-            `Review for ${newReview.product} has been written to JSON file`
-          )
-    );
+
+    // read file
+    try {
+      const file = await fs.readFile("./db/reviews.json", {
+        encoding: "utf-8",
+      });
+      console.log("SUCCESS", file);
+    } catch (err) {
+      console.log("ERR", err);
+    }
+    // if file exists
+    // 1. read contents
+    // 2. append contents with new review
+    // write file
+
+    // if it dosen't exist
+    // just write file
+
+    // fs.writeFile(`./db/reviews.json`, reviewString, (err) =>
+    //   err
+    //     ? console.error(err)
+    //     : console.log(
+    //         `Review for ${newReview.product} has been written to JSON file`
+    //       )
+    // );
 
     const response = {
-      status: 'success',
+      status: "success",
       body: newReview,
     };
 
     console.log(response);
     res.status(201).json(response);
   } else {
-    res.status(500).json('Error in posting review');
+    res.status(500).json("Error in posting review");
   }
 });
 
